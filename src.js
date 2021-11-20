4829ffeb929a52eba24df2d831fddbb7659357f5
Johdasgran <dampig14@gmail.com>
2021-11-20 03:21:35 -0500

    haha ready

--- src.js
+++ src.js
@@ -3,6 +3,121 @@
 
 console.log("patetic xd");
 
+CITAS = [{
+    especialidad: "odontologia",
+    nombre: "Dr. Juan",
+    fecha: "2020-01-01",
+    hora: "08:00"
+},
+{
+    especialidad: "medicina",
+    nombre: "Dr. Mario",
+    fecha: "2020-01-01",
+    hora: "08:00"
+},
+{
+    especialidad: "optometria",
+    nombre: "Dr. Juan",
+    fecha: "2020-01-01",
+    hora: "08:00"
+}
+]
+
+
+// var fecha = new Date();
+
+//     console.log(fecha.getFullYear());
+
+var y = new Date();
+var x = new Date(2020,01,01);
+
+// CITAS[0].hora
+// var mensaje = "Estoy aprendiendo JavaScript y estoy Aprendiendo mucho";
+
+var mensaje = CITAS[0].hora;
+
+var resultado = mensaje.split(":");
+
+console.log(resultado[0]);
+
+console.log(parseInt(resultado));
+
+
+// console.log(y.getTime());
+
+
+
+
+var arrayDeCadenas = CITAS[0].hora.split(", ");
+
+console.log(arrayDeCadenas[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// compararFecha = x.getFullYear()+"-0"+x.getMonth()+"-0"+x.getDate();
+
+
+// if (Date.parse(compararFecha) > Date.parse(CITAS[0].fecha)) {
+//     console.log("WHUALAAA XD");
+// } else {
+    
+// }
+
+// console.log(Date.parse(compararFecha));
+
+
+// console.log(CITAS[0].hora);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -12,37 +127,9 @@ const obtenerCitasDisponibles = (especialidad, fecha_inicio, fecha_final) => {
 
 
 
-    CITAS = [{
-        especialidad: "odontologia",
-        nombre: "Dr. Juan",
-        fecha: "2020-01-01",
-        hora: "08:00"
-    },
-    {
-        especialidad: "medicina",
-        nombre: "Dr. Mario",
-        fecha: "2020-01-01",
-        hora: "08:00"
-    },
-    {
-        especialidad: "optometria",
-        nombre: "Dr. Juan",
-        fecha: "2020-01-01",
-        hora: "08:00"
-    }
-]
 
 
-    console.log(CITAS[0].fecha)
-
-
-
-// bueh por ahí va xd
-
-    var fecha = new Date();
-
-    let fecha1 = Date.parse(CITAS.fecha_inicio);
-    console.log(CITAS.fecha1);
+    // console.log(CITAS[0].fecha)
 
 
 
@@ -67,3 +154,21 @@ const obtenerCitasPorJornada = (especialidad, fecha_inicio, fecha_final, jornada
 
 // module.exports.obtenerCitasDisponibles = obtenerCitasDisponibles;
 // module.exports.obtenerCitasPorJornada = obtenerCitasPorJornada;
+
+
+
+
+
+
+
+// var y = new Date();
+// var x = new Date(2020,01,01);
+
+// compararFecha = x.getFullYear()+"-0"+x.getMonth()+"-0"+x.getDate();
+
+
+// if (Date.parse(compararFecha) > Date.parse(CITAS[0].fecha)) {
+//     console.log("WHUALAAA XD");
+// } else {
+    
+// }
