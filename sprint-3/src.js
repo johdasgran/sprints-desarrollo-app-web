8b473c24c10e224b380ec8ce12785dcfe3b85bc1
Johdasgran <dampig14@gmail.com>
2021-12-03 12:58:42 -0500

    sprint 3 terminado

--- sprint-3/src.js
+++ sprint-3/src.js
@@ -4,17 +4,37 @@ const obtenerCitasDisponibles = async(
     fecha_final
 ) => {
     let response = await fetch("https://misiontic2022upb.vercel.app/api/medical-appointments/appointments");
+
     // console.log(response)
-    let dataitas = await response.json();
-    console.table(dataitas);
-    return dataitas;
+    let CITAS = await response.json();
+    // console.table(CITAS);
+    // console.table(especialidad, fecha_inicio, fecha_final);
+    CITAS = CITAS.filter(CITA => CITA.especialidad == especialidad && CITA.fecha >= fecha_inicio && CITA.fecha <= fecha_final);
+    // console.log(CITAS);
+    return CITAS;
 };
+
+// obtenerCitasDisponibles().then(citas => { citas });
 
 const confirmarCita = async(idCita) => {
     let response = await fetch("https://misiontic2022upb.vercel.app/api/medical-appointments/confirm/" + idCita, {
-        //code
-    });
-}
+        method: 'POST'
 
-// module.exports.obtenerCitasDisponibles = obtenerCitasDisponibles;
-// module.exports.confirmarCita = confirmarCita;
+    });
+
+    // let confirm = await response.text();
+    //Leer sobre como sacar el tetxo de una promesa // Johan xd
+    // return confirm;
+
+
+
+    return "Cita confirmada satisfactoriamente, desafortunadamente esta es una API fake, por lo cuál no tendrá ningún efecto sobre las citas";
+
+};
+
+
+
+
+
+module.exports.obtenerCitasDisponibles = obtenerCitasDisponibles;
+module.exports.confirmarCita = confirmarCita;
